71a21c959ce4da1378e7308edbc2abbe8d37beb1
HrushikeshMiddela <middelahrushikeshsai@gmail.com>
2025-07-23 08:45:06 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/AvatarLab-G432-PS25',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
